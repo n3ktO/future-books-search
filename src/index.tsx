492abf5a80745d740e761b33f81a4c8bd3bde05d
n3ktO <ruslannekto@gmail.com>
2021-07-15 12:54:20 +0300

    Add index.js.

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
+
+import App from './App';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  body {
+    margin: 0;
+  }
+`;
+
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root')
+);

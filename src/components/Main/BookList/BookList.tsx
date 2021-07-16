e18e8803ba8061b4058abb08bf531acff23c7462
n3ktO <ruslannekto@gmail.com>
2021-07-16 22:51:01 +0300

    Add book list styles.

--- src/components/Main/BookList/BookList.tsx
+++ src/components/Main/BookList/BookList.tsx
@@ -1,24 +1,57 @@
 import { observer } from 'mobx-react-lite';
+import styled from 'styled-components';
 
 import BookCard from './BookCard';
 import Button from '../../Button';
 
 import BooksStore from '../../../store/BooksStore';
 
+const BookListWrapperStyle = styled.div`
+width: 100%;
+padding: 0 16px;
+box-sizing: border-box;
+`;
+
+const TotalCountStyle = styled.div`
+width: 100%;
+margin: 16px 0;
+box-sizing: border-box;
+font-weight: 500;
+`;
+
+const BookListStyle = styled.div`
+width: 100%;
+box-sizing: border-box;
+display: grid;
+grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+gap: 16px;
+`;
+
 const LoadMoreButton = styled(Button)`
 width: 100%;
 margin: 16px 0;
 `;
+
 const BookList = observer(() => {
   const { books, total, loadNextBooks, isLoading } = BooksStore;
 
   return (
-    <div>
-      <div>{total}</div>
-      <div>
+    <BookListWrapperStyle>
+      <TotalCountStyle>
+        {isLoading ?
+          'Loading...' :
+          total === null ?
+            'Enter the book you are looking for.' :
+            books.length === 0 ?
+              'No results found.' :
+              `Found ${total} results`
+        }
+      </TotalCountStyle>
+      <BookListStyle>
         {books.map((book, index) => (
           <BookCard key={index} book={book} />
         ))}
+      </BookListStyle>
       {books.length !== 0 && books.length < total && (
         <LoadMoreButton
           onClick={loadNextBooks}
@@ -27,6 +60,7 @@ const BookList = observer(() => {
           Загрузить еще
         </LoadMoreButton>
       )}
+    </BookListWrapperStyle>
   );
 });
 
